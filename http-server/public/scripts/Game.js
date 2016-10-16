6af699accb57b364195a03308dd61dbe84cc7ae6
alexis-valois <alexis.valois@hotmail.com>
2016-10-16 14:35:59 -0400

    animation completed

--- http-server/public/scripts/Game.js
+++ http-server/public/scripts/Game.js
@@ -6,6 +6,7 @@ var Rej = Rej || {};
 Rej.Game = function(){};
 
 var jumpTimer = 0;
+var previousYvelocity;
 
 Rej.Game.prototype = {
   create: function() {
@@ -22,8 +23,8 @@ Rej.Game.prototype = {
    	this.player = this.game.add.sprite(150, 350, 'rej');
   	this.player.animations.add('idle', _.range(0,29), 24, true);
     this.player.animations.add('walk', _.range(30,62), 40, true);
-    this.player.animations.add('jump', _.range(63,78), 40, false);
-    this.player.animations.add('land', _.range(79,91), 40, false);
+    this.player.animations.add('jump', _.range(63,78), 60, false);
+    this.player.animations.add('land', _.range(79,91), 60, false);
   	this.playerScore = 0;
   	this.game.physics.arcade.enable(this.player);
   	this.playerSpeed = 120;
@@ -38,9 +39,13 @@ Rej.Game.prototype = {
   },
 
   update: function() {
-  	var jump;
-    this.game.physics.arcade.collide(this.player, this.layer);
+  	this.game.physics.arcade.collide(this.player, this.layer);
     this.player.body.velocity.x = 0;
+
+    if (previousYvelocity != 0 && this.player.body.onFloor()){
+      console.log("land");
+      this.player.animations.play('land');
+    }
 
     if (this.forward.isDown) {
       this.player.body.velocity.x = +250;
@@ -50,21 +55,21 @@ Rej.Game.prototype = {
       this.player.body.velocity.x = -250;
     }
 
-    if (this.player.body.velocity.x > 0  && this.player.body.onFloor()){
-    	if (!this.player.animations.getAnimation('jump').isPlaying){
+    if (this.player.body.velocity.x > 0 ){
+    	if (!this.player.animations.getAnimation('jump').isPlaying && !this.player.animations.getAnimation('land').isPlaying && this.player.body.onFloor()){
     		this.player.animations.play('walk');
     	} 
-      	this.player.scale.x = 1;
+      this.player.scale.x = 1;
     }
 
-    if (this.player.body.velocity.x < 0  && this.player.body.onFloor()){
-    	if (!this.player.animations.getAnimation('jump').isPlaying){
+    if (this.player.body.velocity.x < 0 ){
+    	if (!this.player.animations.getAnimation('jump').isPlaying && !this.player.animations.getAnimation('land').isPlaying && this.player.body.onFloor()){
     		this.player.animations.play('walk');
     	}    	
-      	this.player.scale.x = -1;
+      this.player.scale.x = -1;
     }
 
-    if (!this.player.animations.getAnimation('jump').isPlaying && this.player.body.velocity.x == 0){
+    if (!this.player.animations.getAnimation('jump').isPlaying && !this.player.animations.getAnimation('land').isPlaying && this.player.body.velocity.x == 0){
       this.player.animations.play('idle');
     }
 
@@ -74,5 +79,7 @@ Rej.Game.prototype = {
         	jumpTimer = this.game.time.now + 750;
     	}, this);
     }
+
+    previousYvelocity = this.player.body.velocity.y;
   },
 };
